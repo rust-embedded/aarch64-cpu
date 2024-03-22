a50337663bcc83a07836ed75e692314f773f0f39
varia <varia07@sjtu.edu.cn>
2024-03-22 15:07:22 +0800

    feat(exception): add a new exception reason

--- src/exception.rs
+++ src/exception.rs
@@ -1,13 +1,15 @@
 #[derive(Copy, Clone)]
 pub enum ExceptionReason {
-    DataAbort = 0x24,
+    DataAbortLowerEl = 0b100100,
+    DataAbortCurrentEl = 0b100101,
     Unimplemented = 0xff,
 }
 
 impl From<u64> for ExceptionReason{
     fn from(value: u64) -> Self {
         match value {
-            x if x == Self::DataAbort as u64 => Self::DataAbort,
+            x if x == Self::DataAbortLowerEl as u64 => Self::DataAbortLowerEl,
+            x if x == Self::DataAbortCurrentEl as u64 => Self::DataAbortCurrentEl,
             _ => Self::Unimplemented
         }
     }
