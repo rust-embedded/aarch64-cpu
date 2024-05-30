a1393c2c0644cc13cc24a72ffc176b13d13d9a8b
Sangwan Kwon <sangwan.kwon@samsung.com>
2024-05-30 15:40:04 +0900

    Add ICH_AP0Rn_EL2, ICH_AP1Rn_EL2 registers
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

---
+++ src/registers/ich_ap0r3_el2.rs
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//
+// Copyright (c) 2024 by the author(s)
+//
+// Author(s):
+//   - Sangwan Kwon <sangwan.kwon@samsung.com>
+
+//! Interrupt Controller Hyp Active Priorities Group 0 Registers - EL2
+//!
+//! Provides information about Group 0 virtual active priorities for EL2.
+
+use tock_registers::interfaces::{Readable, Writeable};
+
+pub struct Reg;
+
+impl Readable for Reg {
+    type T = u64;
+    type R = ();
+
+    sys_coproc_read_raw!(u64, "ICH_AP0R3_EL2", "x");
+}
+
+impl Writeable for Reg {
+    type T = u64;
+    type R = ();
+
+    sys_coproc_write_raw!(u64, "ICH_AP0R3_EL2", "x");
+}
+
+pub const ICH_AP0R3_EL2: Reg = Reg {};
