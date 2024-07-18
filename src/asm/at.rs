20a86dcec093da817f895ee7909a1c3e0f1dd409
Yiyang Wu <toolmanp@tlmp.cc>
2024-07-18 14:06:03 +0800

    feat: add translation command
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- src/asm/at.rs
+++ src/asm/at.rs
@@ -1,3 +1,5 @@
+use crate::registers::*;
+
 mod sealed {
     pub trait At {
         fn at(&self, va: u64);
@@ -27,8 +29,20 @@ macro_rules! at {
 }
 
 at!(S1E1R, S1e1r);
+at!(S1E2R, S1e2r);
 
 #[inline(always)]
-pub fn at(_arg:impl sealed::At , va:u64){
+pub fn at(_arg: impl sealed::At, va: u64) {
     _arg.at(va);
+}
+
+#[inline(always)]
+pub fn translate(_arg: impl sealed::At, va: u64) -> Option<u64> {
+    _arg.at(va);
+    let par = PAR_EL1.get();
+    if PAR_EL1::F.read(par) == PAR_EL1::F::TranslationAborted.value {
+        None
+    } else {
+        Some(PAR_EL1::PA.read(par) << 12)
+    }
 }
