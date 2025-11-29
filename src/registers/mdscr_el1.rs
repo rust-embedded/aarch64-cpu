d5e14bef8b3572e12c2b407e121e0bf7892113b9
Berkus Decker <berkus@users.noreply.github.com>
2025-11-29 14:42:45 +0200

    Update documentation comment

--- src/registers/mdscr_el1.rs
+++ src/registers/mdscr_el1.rs
@@ -8,6 +8,8 @@
 //! Monitor Debug System Control Register - EL1
 //!
 //! Main control register for debug functions.
+//!
+//! This register is present only when FEAT_AA64 is implemented. Otherwise, direct accesses to MDSCR_EL1 are UNDEFINED.
 
 use tock_registers::{
     interfaces::{Readable, Writeable},
