2fa63909c0abb80987f0dd12823272f566eafc71
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2026-02-23 14:59:27 +0000

    Adds an example program for Armv8-R AArch64 - the 64-bit version of Armv8-R.
    The example can be executed inside the free Arm Base Architecture
    Envelope Model (AEM) Fixed Virtual Platform (FVP), available from
    https://developer.arm.com/Tools%20and%20Software/Fixed%20Virtual%20Platforms/Arm%20Architecture%20FVPs.

---
+++ examples/armv8-r/src/bin/hello.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+use core::sync::atomic::{self, AtomicU64};
+
+use aarch64_cpu::registers::{self, Readable};
+use aarch64_rt::entry;
+use semihosting::{println, process};
+
+static X: AtomicU64 = AtomicU64::new(0);
+static Y: AtomicU64 = AtomicU64::new(1);
+
+entry!(main);
+
+fn main(_arg0: u64, _arg1: u64, _arg2: u64, _arg3: u64) -> ! {
+    let el = registers::CurrentEL.read(registers::CurrentEL::EL);
+    println!("Hello, world! running from EL{el}");
+
+    let x = X.fetch_add(1, atomic::Ordering::Relaxed);
+    let y = Y.fetch_add(1, atomic::Ordering::Relaxed);
+    println!("static variables: X={x}, Y={y}");
+
+    // sanity check that static variables are initialized by `aarch64-rt`
+    assert_eq!(0, x, ".bss was not initialized");
+    assert_eq!(1, y, ".data was not initialized");
+
+    process::exit(0)
+}
