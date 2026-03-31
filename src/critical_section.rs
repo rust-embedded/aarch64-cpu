efaa72fefff3ec77add8a3b2441a15a190619f28
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2026-03-31 17:47:39 +0100

    critical_section impl uses new DAIFSet/DAIFClr API.
    Saves a read/modify/write loop.

--- src/critical_section.rs
+++ src/critical_section.rs
@@ -12,27 +12,27 @@ mod single_core {
 
     unsafe impl critical_section::Impl for SingleCoreCriticalSection {
         unsafe fn acquire() -> critical_section::RawRestoreState {
-            let daif_bits = registers::DAIF.get();
+            // get which of DAIF are currently masked (shift to fit into `u8`)
+            let daif_bits = registers::DAIF.get() >> DAIF_OFFSET;
 
-            registers::DAIF.write(
-                registers::DAIF::D::Masked
-                    + registers::DAIF::A::Masked
-                    + registers::DAIF::I::Masked
-                    + registers::DAIF::F::Masked,
-            );
+            // mask everything (even if already masked)
+            registers::DAIFSet.set(0b1111);
 
             // prevent reordering across the preceding register write
             atomic::compiler_fence(atomic::Ordering::SeqCst);
 
-            // shift DAIF value (only 4 contiguous bits are used) to fit into `u8`
-            (daif_bits >> DAIF_OFFSET) as u8
+            // record which DAIF bits we need to clear later (only clear the ones that were not
+            // masked)
+            let bits_to_clear = daif_bits ^ 0b1111;
+            bits_to_clear as u8
         }
 
-        unsafe fn release(daif_bits: critical_section::RawRestoreState) {
+        unsafe fn release(bits_to_clear: critical_section::RawRestoreState) {
             // prevent reordering across the following register write
             atomic::compiler_fence(atomic::Ordering::SeqCst);
 
-            registers::DAIF.set((daif_bits << DAIF_OFFSET) as u64);
+            // unmask the relevant DAIF bits
+            registers::DAIFClr.set(bits_to_clear as u64);
         }
     }
 }
