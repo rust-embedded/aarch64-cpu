4bec62566fa2575b1b299812f422cde3d7f1f0b9
CUB3D <callumthom11@gmail.com>
2026-04-09 14:07:31 +0100

    Update id_aa64isar0_el1 and id_aa64isar1_el1

--- src/registers/id_aa64isar0_el1.rs
+++ src/registers/id_aa64isar0_el1.rs
@@ -4,6 +4,7 @@
 //
 // Author(s):
 //   - Ali Saidi <alisaidi@amazon.com>
+//   - Callum Thomson <callumthom11@gmail.com>
 
 //! AArch64 Instruction Set Architecture Feature Register 0 - EL1
 //!
@@ -21,7 +22,96 @@ register_bitfields! {u64,
         /// All other values are reserved.
         RNDR OFFSET(60) NUMBITS(4) [
             Supported = 0b0001,
-            NotSupported = 0b0000
+            NotSupported = 0b0000,
+        ],
+       
+        /// Support for Outer Sharable and TLB range maintenance instructions
+        TLB OFFSET(56) NUMBITS(4) [
+            NotImplemented = 0b0000,
+            OuterShareable = 0b0001,
+            Implemented = 0b0010,
+        ],
+
+        /// Support for flag manipulation instructions
+        TS OFFSET(52) NUMBITS(4) [
+            NotImplemented = 0b0000,
+            Partial = 0b0001,
+            Full = 0b0010,
+        ],
+
+        /// Support for FMAL and FMLSL instructions
+        FHM OFFSET(48) NUMBITS(4) [
+            NotImplemented = 0b0000,
+            Implemented = 0b0001,
+        ],
+
+        /// Support for Dot product instructions
+        DP OFFSET(44) NUMBITS(4) [
+            NotImplemented = 0b0000,
+            Implemented = 0b0001,
+        ],
+
+        /// Support for SM4 instructions
+        SM4 OFFSET(40) NUMBITS(4) [
+            NotImplemented = 0b0000,
+            Implemented = 0b0001,
+        ],
+
+        /// Support for SM3 instructions
+        SM3 OFFSET(36) NUMBITS(4) [
+            NotImplemented = 0b0000,
+            Implemented = 0b0001,
+        ],
+
+        /// Support for SHA3 instructions
+        SHA3 OFFSET(32) NUMBITS(4) [
+            NotImplemented = 0b0000,
+            Implemented = 0b0001,
+        ],
+
+        /// Support for SQRDMLAH and SQRDMLSH instructions
+        RDM OFFSET(28) NUMBITS(4) [
+            NotImplemented = 0b0000,
+            Implemented = 0b0001,
+        ],
+
+        /// Support for TME instructions
+        TME OFFSET(24) NUMBITS(4) [
+            NotImplemented = 0b0000,
+            Implemented = 0b0001,
+        ],
+
+        /// Support for Atomic instructions
+        Atomic OFFSET(20) NUMBITS(4) [
+            NotImplemented = 0b0000,
+            Partial = 0b0001,
+            Full = 0b0010,
+        ],
+
+        /// Support for CRC32 instructions
+        CRC32 OFFSET(16) NUMBITS(4) [
+            NotImplemented = 0b0000,
+            Implemented = 0b0001,
+        ],
+
+        /// Support for SHA2 instructions
+        SHA2 OFFSET(12) NUMBITS(4) [
+            NotImplemented = 0b0000,
+            Partial = 0b0001,
+            Full = 0b0010,
+        ],
+
+        /// Support for SHA1 instructions
+        SHA1 OFFSET(8) NUMBITS(4) [
+            NotImplemented = 0b0000,
+            Implemented = 0b0001,
+        ],
+
+        /// Support for AES instructions
+        AES OFFSET(4) NUMBITS(4) [
+            NotImplemented = 0b0000,
+            Partial = 0b0001,
+            Full = 0b0010,
         ],
     ]
 }
